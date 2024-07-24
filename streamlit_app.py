6b2aa7e71de2b11ba93f6864ba84565afeeee261
projjal123 <102586989+projjal123@users.noreply.github.com>
2024-07-24 11:04:58 +0530

    Update streamlit_app.py
    added api information from fruityvice

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,5 +42,10 @@ if ingredients_list:
             session.sql(my_insert_stmt).collect()       
             st.success(f'Your Smoothie is ordered!,{name_on_order}!', icon="✅")
 
+#new session to display fruityvice nutriotion information
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+st.text(fruityvice_response)
+
 
         
