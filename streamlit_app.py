86b27106f3b8b1e3d196108c24934c948b0e1a4d
projjal123 <102586989+projjal123@users.noreply.github.com>
2024-07-24 16:07:58 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,9 +1,7 @@
 # Import python packages
 import streamlit as st
-#from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
-import requests
-import pandas as pd
 
 # Write directly to the app
 st.title(":cup_with_straw: Customize your smoothie :cup_with_straw:")
@@ -16,20 +14,12 @@ name_on_order = st.text_input('Name on Smoothie:')
 st.write('The name on your order will be: ',name_on_order)
 
 
-cnx = st.connection("snowflake")
-session = cnx.session()
-#session = get_active_session()
-my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
-#st.stop()
-pd_df=my_dataframe.to_pandas()
-# Convert the 'FRUIT_NAME' column to a list
-fruit_names = pd_df['FRUIT_NAME'].tolist()
 
-#st.dataframe(pd_df)
-#st.stop()
-
-ingredients_list = st.multiselect('Choose up to 5 ingredients :',fruit_names, max_selections=5 )
+ingredients_list = st.multiselect('Choose up to 5 ingredients :',my_dataframe, max_selections=5 )
 
 if ingredients_list:
     #st.write(ingredients_list)
@@ -38,11 +28,6 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
-        st.subheader(fruit_chosen + 'Nutrition Information')
-        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
-        fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
     my_insert_stmt = f""" 
@@ -55,10 +40,6 @@ if ingredients_list:
     if time_to_insert:
             session.sql(my_insert_stmt).collect()       
             st.success(f'Your Smoothie is ordered!,{name_on_order}!', icon="✅")
-
-
-
-
 
 
         
