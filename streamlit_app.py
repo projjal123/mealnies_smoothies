bc44baa422da7233b1fa85468670908f86022764
projjal123 <102586989+projjal123@users.noreply.github.com>
2024-07-24 11:10:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,7 +45,7 @@ if ingredients_list:
 #new session to display fruityvice nutriotion information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
 
 
         
